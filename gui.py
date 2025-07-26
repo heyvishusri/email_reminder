ea958a325f7bdd12255e0d916e72d88ef49254e5
heyvishusri <vishwaskumar55347@gmail.com>
2025-07-26 15:59:24 +0530

    updated

---
+++ gui.py
@@ -0,0 +1,44 @@
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QDateTimeEdit, QLineEdit, QTextEdit
+import sys
+from PyQt6.QtCore import QDateTime
+
+
+
+class ReminderApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Add Reminder")
+        self.setup_ui()
+
+    def setup_ui(self):
+        layout = QVBoxLayout()
+
+        #Event date and time
+        self.datetime_label = QLabel("Event Date and Time")
+        self.datetime_picker = QDateTimeEdit()
+        self.datetime_picker.setDateTime(QDateTime.currentDateTime())
+        self.datetime_picker.setCalendarPopup(True)
+        layout.addWidget(self.datetime_label)
+        layout.addWidget(self.datetime_picker)
+
+        #Email
+        self.email_label = QLabel("Email: ")
+        self.email_input = QLineEdit("vkumar080@rku.ac.in")
+        layout.addWidget(self.email_label)
+        layout.addWidget(self.email_input)
+
+        #Reminder
+        self.reminder_label = QLabel("Reminder: ")
+        self.reminder_input = QTextEdit()
+        layout.addWidget(self.reminder_label)
+        layout.addWidget(self.reminder_input)
+
+
+
+
+        self.setLayout(layout)
+
+app = QApplication(sys.argv)
+window = ReminderApp()
+window.show()
+sys.exit(app.exec())
